07fe126afebdd40bc75e48b570064f90794c2b70
Denis Cooper <denis.cooper09@gmail.com>
2023-05-07 21:18:38 +0100

    Add SFML and basic window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+
+	sf::VideoMode video_mode(1000, 800);
+
+	sf::RenderWindow window(video_mode, "Sierpinski Triangle Generator",sf::Style::Default, settings);
+
+	while (window.isOpen()) {
+		sf::Event event{};
+
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		window.clear(sf::Color::White);
+		window.display();
+	}
+
 	return 0;
 }
